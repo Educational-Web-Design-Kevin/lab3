3f635527a0002a37f00199b2295ea7776b984507
Kevin-Alexis-Aguilar <89360177+Kevin-Alexis-Aguilar@users.noreply.github.com>
2023-10-04 13:19:43 -0600

    Remove bad syntax javascript code in site file

--- js/site.js
+++ js/site.js
@@ -3,11 +3,3 @@ jQuery(function($) {
     $('html').removeClass('nojs');
     $('html').addClass('hasjs');
   });
-/*Credit: Rick Bradshaw*/
-/*https://stackoverflow.com/questions/2851663/how-do-i-simulate-a-hover-with-a-touch-in-touch-enabled-browsers*/
-$(document).ready(function() {
-    $('.hover').on('touchstart touchend', function(e) {
-        e.preventDefault();
-        $(this).toggleClass('hover_effect');
-    });
-});
